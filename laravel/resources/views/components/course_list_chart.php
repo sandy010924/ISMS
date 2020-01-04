1f7d5504db5fa0887805e69efeedd8f5e3c740cb
joanna <okokis101@gmail.com>
2020-01-05 00:21:35 +0800

    把圖表變成component形式加入course_list_chart

---
+++ laravel/resources/views/components/course_list_chart.php
@@ -0,0 +1,53 @@
+<!-- Content Start -->
+<!--搜尋課程頁面內容-->
+<div class="card mt-3 mb-3 container">
+  <div class="card-body">
+    <!-- content -->
+    <div class="content_box">
+      <!-- 詳細報名資訊 -->
+      <div class="mt-3 mb-3">
+        <div class="card custom_card">
+          <div class="card-header custom_card_header">
+            <h5>2019/11/19(二)</h5>
+            <h5>零秒成交術零秒成交術</h5>
+            <h5>台北下午場</h5>
+            <small>天氣：下雨</small>
+            <br>
+            <small>備註：學員姍姍來遲</small>
+          </div>
+          <div class="card-body custom_card_body">
+            <div>
+              <div class="row custom_row">
+                <label class="custom_label">銷講地點：</label>
+                <span>文創</span>
+              </div>
+              <div class="row custom_row">
+                <label class="custom_label">主持開收：</label>
+                <span>Adam</span>
+              </div>
+              <div class="row custom_row">
+                <label class="custom_label">工作人員：</label>
+                <span>Ellen、Sunnie</span>
+              </div>
+              <div class="row custom_row">
+                <label class="custom_label">現場完款：</label>
+                <span>39800</span>
+              </div>
+              <div class="row custom_row">
+                <label class="custom_label">五日內完款：</label>
+                <span>42800</span>
+              </div>
+              <div class="row custom_row">
+                <label class="custom_label">分期付款：</label>
+                <span>52800</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="pie_box"><canvas id="pie_chart_turnover_rate" width="200px" height="200px"></canvas></div>
+      <div class="pie_box"><canvas id="pie-chart_check_in_rate" width="200px" height="200px"></canvas></div>
+    </div>
+  </div>
+</div>
+<!-- Content End -->
--- laravel/resources/views/frontend/layouts/master.blade.php
+++ laravel/resources/views/frontend/layouts/master.blade.php
@@ -7,21 +7,22 @@
   <meta name="description" content="">
   <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
   <meta name="generator" content="Jekyll v3.8.6">
-  
+
   <title>@yield('title') | 無極限學員系統</title>
 
   <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/dashboard/">
-  
+
   <!-- Bootstrap core CSS -->
   <link href="https://getbootstrap.com/docs/4.4/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  
+
     <!-- Fontawesome Icon -->
   <link href="{{ asset('font-awesome/css/all.css') }}/" rel="stylesheet">
   <!-- Custom styles -->
   <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
   <link href="{{ asset('css/web.css') }}" rel="stylesheet">
   <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/course_list_chart.css') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -31,11 +32,11 @@
       @include('frontend.layouts.navbar')
 
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10">
- 
+
           @include('frontend.layouts.header')
 
           @yield('content')
-        
+
         </main>
     </div>
   </div>
@@ -49,7 +50,8 @@
     integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
     crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
-
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+  <script src="{{ asset('js/course_list_chart.js') }}"></script>
 </body>
 
 </html>
