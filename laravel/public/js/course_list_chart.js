7f12bff6a9d4edb6a8c2aa949b076e4c87b54e71
joanna <okokis101@gmail.com>
2020-02-05 00:25:39 +0800

    更改 課程數據圖表

--- laravel/public/js/course_list_chart.js
+++ laravel/public/js/course_list_chart.js
@@ -6,28 +6,22 @@ Chart.defaults.global.defaultFontFamily = '微軟正黑體';
 var turnover_rate_config = {
   type: 'pie',
   data: {
-    labels: ["成交數", "未成交數"], // 成交數、未成交數 下方呈現 實到人數
+    labels: ["追完款數", "完款數","實到人數"],
     datasets: [{
-      backgroundColor: ["#F9A03F", "#D45113"],
-      data: [3,25]
+      backgroundColor: ["#F9A03F", "#D45113", "#f24"],
+      data: [4,3,25]
     }]
   },
   options: {
-  //   layout: {
-  //     padding: {
-  //         left: 0,
-  //         right: 0,
-  //         top: 0,
-  //         bottom: 0
-  //     }
-  // },
     title: {
       display: true,
-      text: '成交率: 0.12%',
-      fontSize: '26'
+      text: ['成交率: 0.12%','追款率: 16%','完款率: 12%'],
+      fontSize: '20'
     },
     legend: {
-      display: true,
+      display: false,
+      width: 300,
+      height: 300,
       labels: {
         generateLabels: function (chart) {
             var data = chart.data;
@@ -69,20 +63,22 @@ const pie_chart_turnover_rate = new Chart(ctx_turnover_rate, turnover_rate_confi
 var check_in_rate_config = {
   type: 'pie',
   data: {
-    labels: ["實到人數","未到人數", "取消人數"], // 實到、未到、取消 下方呈現 報名比數
+    labels: ["實到人數","報名筆數"], // 實到、未到、取消 下方呈現 報名比數
     datasets: [{
-      backgroundColor: ["#16F4D0","#429EA6","#153B50"],
-      data: [25,7,67]
+      backgroundColor: ["#16F4D0","#153B50"],
+      data: [25,67]
     }]
   },
   options: {
     title: {
       display: true,
-      text: '報到率: 41.6%',
-      fontSize: '26'
+      text: ['','報到率: 41.6%',''],
+      fontSize: '20'
     },
     legend: {
-      display: true,
+      display: false,
+      width: 300,
+      height: 300,
       labels: {
         generateLabels: function (chart) {
             var data = chart.data;
--- laravel/resources/views/components/course_list_chart.php
+++ laravel/resources/views/components/course_list_chart.php
@@ -11,24 +11,28 @@
             <h5>2019/11/19(二)</h5>
             <h5>零秒成交術零秒成交術</h5>
             <h5>台北下午場</h5>
+            <div class="row custom_row">
+                <label class="custom_label">銷講地點：</label>
+                <span>文創</span>
+            </div>
+            <div class="row custom_row">
+                <label class="custom_label">結束收單：</label>
+                <span>Adam</span>
+            </div>
+            <div class="row custom_row">
+                <label class="custom_label">主持開場：</label>
+                <span>Adam</span>
+            </div>
+            <div class="row custom_row">
+                <label class="custom_label">工作人員：</label>
+                <span>Ellen、Sunnie</span>
+            </div>
             <small>天氣：下雨</small>
             <br>
             <small>備註：學員姍姍來遲</small>
           </div>
           <div class="card-body custom_card_body">
             <div>
-              <div class="row custom_row">
-                <label class="custom_label">銷講地點：</label>
-                <span>文創</span>
-              </div>
-              <div class="row custom_row">
-                <label class="custom_label">主持開收：</label>
-                <span>Adam</span>
-              </div>
-              <div class="row custom_row">
-                <label class="custom_label">工作人員：</label>
-                <span>Ellen、Sunnie</span>
-              </div>
               <div class="row custom_row">
                 <label class="custom_label">現場完款：</label>
                 <span>39800</span>
@@ -41,12 +45,31 @@
                 <label class="custom_label">分期付款：</label>
                 <span>52800</span>
               </div>
+              <div class="row custom_row">
+                <label class="custom_label">完款數：</label>
+                <span>3</span>
+              </div>
+              <div class="row custom_row">
+                <label class="custom_label">付訂數：</label>
+                <span>2</span>
+              </div>
+              <div class="row custom_row">
+                <label class="custom_label">留單數：</label>
+                <span>3</span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="pie_box"><canvas id="pie_chart_turnover_rate" width="200px" height="200px"></canvas></div>
-      <div class="pie_box"><canvas id="pie-chart_check_in_rate" width="200px" height="200px"></canvas></div>
+      <div style="width: 400px; height: 400px">
+        <div class="pie_box"><canvas id="pie_chart_turnover_rate" width="200px" height="200px"></canvas></div>
+        <div style="text-align: center">實到人數: 25</div>
+      </div>
+      <div style="width: 400px; height: 400px">
+        <div class="pie_box"><canvas id="pie-chart_check_in_rate" width="200px" height="200px"></canvas></div>
+        <div style="text-align: center">實到人數: 25</div>
+        <div style="text-align: center">取消筆數: 07</div>
+      </div>
     </div>
   </div>
   <!-- <input id="refresh" type="button" value="更新圖表"></button> -->
