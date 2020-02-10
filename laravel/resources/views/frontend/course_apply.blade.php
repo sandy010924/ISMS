a2f47fd5393e702fee1f701b94f8e8a2b9b2b08b
Yizhen <c0925871129@gmail.com>
2020-02-10 15:51:51 +0800

    [查詢名單]頁面修改

--- laravel/resources/views/frontend/course_apply.blade.php
+++ laravel/resources/views/frontend/course_apply.blade.php
@@ -4,8 +4,86 @@
 @section('header', '查詢名單')
 
 @section('content')
+
 <!-- Content Start -->
         <!--查看報名內容-->
+        <div class="card m-3">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-3 align-middle">
+                <input type="hidden" id="course_id" value="{{ $course->id }}">
+                <h6>
+                      講師名稱 : <input type="text" class="mt-2" readonly>
+                </h6>
+              </div>
+              <div class="col-3 align-middle">
+                <h6>
+                    課程名稱 : <input type="text" class="mt-2" readonly>
+                </h6>
+              </div>
+              <hr/>
+            </div>
+          </div>
+        </div>
+        <div class="card m-3">
+            <div class="card-body">
+                <div class="row mb-3">
+                    <div class="col-6 p-2 ml-2">
+                        <h6 class="mb-0">2019/12/31(五)&nbsp;&nbsp;台北下午場&nbsp;&nbsp;即時報名筆數:45&nbsp;&nbsp;取消報名比數:7</h6>
+                    </div>
+                    <div class="col-2">
+                    </div>
+                    <div class="col-3 align-right">
+                      <div class="input-group">
+                          <input type="number" class="form-control" placeholder="關鍵字" id="search_phone" max="999">
+                          <div class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="button" id="btn_search">搜尋</button>
+                          </div>
+                      </div>
+                    </div>
+                </div>
+                <div class="table-responsive">
+                  <table class="table table-striped table-sm text-center" id="table_apply">
+                    <thead>
+                      <tr>
+                        <th>Submission Date</th>
+                        <th>名單來源</th>
+                        <th>姓名</th>
+                        <th>聯絡電話</th>
+                        <th>電子郵件</th>
+                        <th>目前職業</th>
+                        <th>我想在講座中了解的內容</th>
+                        <th>報到</th>
+                        <th>付款狀態</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody id="apply_list">
+                      @foreach($courseapplys as $courseapply)
+                        <tr>
+                          <td></td>
+                          <td></td>
+                          <td>{{ $courseapply->name }}</td>
+                          <td>{{ $courseapply->phone }}</td>
+                          <td>{{ $courseapply->email }}</td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td>
+                            <div class="dropdown">
+                              <button type="button" name="apply_status" class="btn btn-sm text-white apply_btn" id="{{ $courseapply->apply_id }}" value="{{ $courseapply->apply_status_val }}">{{ $courseapply->apply_status_name }}</button>
+                            </div>
+                          </td>
+                        </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+
+        <!--舊程式碼
         <div class="card m-3">
           <div class="card-body">
             <div class="row">
@@ -68,7 +146,7 @@
                   </table>
                 </div>
               </div>
-            </div>
+            </div>-->
           <!-- Content End -->
 
   <script>
@@ -128,4 +206,6 @@
     //列表搜尋end
 
   </script>
+
+
 @endsection
