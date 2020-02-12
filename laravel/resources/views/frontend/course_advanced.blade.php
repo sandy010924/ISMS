726c87b3b5ef872ee6ce2d7469b32fb0a3363e53
Yizhen <c0925871129@gmail.com>
2020-02-12 23:52:31 +0800

    [進階填單名單][本日報表]頁面修改

--- laravel/resources/views/frontend/course_advanced.blade.php
+++ laravel/resources/views/frontend/course_advanced.blade.php
@@ -6,40 +6,83 @@
 @section('content')
 <!-- Content Start -->
        <!--搜尋課程頁面內容-->
-        <div class="card m-3">
+       <div class="card m-3">
           <div class="card-body">
-            {{-- <div class="row menu_search"> --}}
-            <div class="row mb-3">
-              <div class="col-3 mx-3">
-                
+            <div class="row">
+              <div class="col-3 align-middle">
+                <h6>
+                      講師名稱 : <input type="text" class="mt-2" readonly>
+                </h6>
               </div>
-              <div class="col"></div>
-              <div class="col-3">
-                <input type="date" class="form-control" id="search_date">
+              <div class="col-3 align-middle">
+                <h6>
+                    課程名稱 : <input type="text" class="mt-2" readonly>
+                </h6>
               </div>
-              <div class="col-3">
-                <input type="search" class="form-control" placeholder="搜尋課程" aria-label="Class's name" id="search_name">
-              </div>
-              <div class="col-2">
-                <button class="btn btn-outline-secondary" type="button" id="btn_search">搜尋</button> 
-              </div>
+              <hr/>
             </div>
+          </div>
+        </div>
+        <div class="card m-3">
+            <div class="card-body">
+                <div class="row mb-3">
+                    <div class="col-6 p-2 ml-2">
+                        <h6 class="mb-0">2019/12/31(五)&nbsp;&nbsp;台北下午場&nbsp;&nbsp;零秒成交數&nbsp;&nbsp;填單筆數:5</h6>
+                    </div>
+                    <div class="col-2">
+                    </div>
+                    <div class="col-3 align-right">
+                      <div class="input-group">
+                          <input type="number" class="form-control" placeholder="關鍵字"  max="999">
+                          <div class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="button">搜尋</button>
+                          </div>
+                      </div>
+                    </div>
+                </div>
             <div class="table-responsive">
               <table class="table table-striped table-sm text-center">
                 <thead>
                   <tr>
-                    <th>日期</th>
-                    <th>課程名稱</th>
-                    <th>場次</th>
-                    <th>報名筆數</th>
-                    <th>實到筆數</th>
+                    <th>Submission Date</th>
+                    <th>報名日期</th>
+                    <th>姓名</th>
+                    <th>聯絡電話</th>
+                    <th>電子郵件</th>
+                    <th>我想參加課程</th>
+                    <th>報名場次</th>
+                    <th></th>
+                    <th></th>
                     <th></th>
                   </tr>
                 </thead>
-                <tbody id="course_list">
-                
-
-                </tbody>
+                <tbody>
+                    <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td>現場最優惠價格</td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td><a href="#"><button type="button" class="btn btn-secondary btn-sm">刪除</button></a></td>
+                    </tr>
+                    <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td>五日內最優惠價格</td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td><a href="#"><button type="button" class="btn btn-secondary btn-sm">刪除</button></a></td>
+                    </tr>
+                    
+                  </tbody>
               </table>
             </div>
           </div>
--- laravel/resources/views/frontend/course_return.blade.php
+++ laravel/resources/views/frontend/course_return.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layouts.master')
 
 @section('title', '課程管理')
-@section('header', '回報表單')
+@section('header', '本日報表')
 
 @section('content')
 <!-- Content Start -->
@@ -9,48 +9,75 @@
         <div class="card m-3">
           <div class="card-body">
             <div class="row mb-3">
-              <div class="col-5">
-                <h5>零秒成交數 2019/11/20 台北下午場</h5>
-              </div>
-              <div class="col-7 text-right">
-                <h5>講座地點 : 台北市金山南路一段17號5樓(博宇藝享空間)</h5>
+              <div class="col text-center">
+                <h6>零秒成交數&nbsp;&nbsp;2019/11/20&nbsp;&nbsp;台北下午場&nbsp;&nbsp;講座地點 : 台北市金山南路一段17號5樓(博宇藝享空間)</h6>
               </div>
             </div>
               <div class="row">
-                <div class="col-2">
-                  <p class="form_text">主持 : </p>
+                <div class="col-3 mb-2">
+                  <h6>主持開場 : </h6>
                 </div>
-                <div class="col-2">
-                  <p class="form_text">開收 : </p>
+                <div class="col-3 mb-2">
+                  <h6>結束收單 : </h6>
+                </div>
+                <div class="col-3 mb-2">
+                  <h6>工作人員 : </h6>
+                </div>
+                <div class="col-3 mb-2">
+                  <h6>天氣 : </h6>
+                </div>
+                
+                
+              </div>
+              <div class="row">
+                
+                <div class="col-3 mb-2">
+                  <h6>該場總金額 : </h6>
                 </div>
                 <div class="col-3">
-                  <p class="form_text">工作人員 : </p>
-                </div>
-                <div class="col-2">
-                  <p class="form_text">天氣 : </p>
+                  <h6>完款 : </h6>
                 </div>
                 <div class="col-3">
-                  <p class="form_text">該場總金額 : </p>
+                  <h6>付訂 : </h6>
+                </div>
+                <div class="col-3">
+                  <h6>留單 : </h6>
                 </div>
               </div>
               <div class="row">
                 <div class="col-3">
-                  <p class="form_text">現場完款 : <input type="text" class="form_input"></p>
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">現場完款</span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="# input" aria-describedby="#">
+                  </div>
                 </div>
                 <div class="col-3">
-                  <p class="form_text">五日內完款 : <input type="text" class="form_input"></p>
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">五日內完款</span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="# input" aria-describedby="#">
+                  </div>
                 </div>
                 <div class="col-3">
-                  <p class="form_text">分期付款 : <input type="text" class="form_input"></p>
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">分期付款</span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="# input" aria-describedby="#">
+                  </div>
                 </div>
-                <div class="col-3">
-                  <p class="form_text">該場備註 : <input type="text" class="form_input"></p>
+                <div class="col-3 mb-2">
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">該場備註</span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="# input" aria-describedby="#">
+                  </div>
                 </div>
-              </div>
-              <div class="row">
-                <div class="col-3 mx-auto">
-                  <button type="button" class="btn btn-secondary  btn-block">儲存</button>
-                </div>
+                
               </div>
             </div>
         </div>
@@ -58,49 +85,88 @@
         <div class="card m-3">
           <div class="card-body">
             <div class="table-responsive">
-              <table class="table table-striped table-sm class_table form_table">
+              <table class="table table-striped table-sm return_table text-center">
                 <thead>
                   <tr>
-                    <th>學員姓名</th>
-                    <th>連絡電話</th>
-                    <th>付款狀態</th>
-                    <th>應付</th>
-                    <th>已付</th>
-                    <th>待付</th>
-                    <th>付款方式1</th>
-                    <th>金額1</th>
-                    <th>帳戶/卡號後四碼1</th>
-                    <th>付款方式2</th>
-                    <th>金額2</th>
-                    <th>帳戶/卡號後四碼2</th>
-                    <th>付款方式3</th>
-                    <th>金額3</th>
-                    <th>帳戶/卡號後四碼3</th>
-                    <th>服務人員</th>
-                    <th>追單人員</th>
-                    <th>備註</th>
+                    <th class="text-nowrap ">學員姓名</th>
+                    <th class="text-nowrap">連絡電話</th>
+                    <th class="text-nowrap">付款狀態</th>
+                    <th class="text-nowrap">應付</th>
+                    <th class="text-nowrap">已付</th>
+                    <th class="text-nowrap">待付</th>
+                    <th class="text-nowrap">付款方式1</th>
+                    <th class="text-nowrap">金額1</th>
+                    <th class="text-nowrap">帳戶/卡號後四碼1</th>
+                    <th class="text-nowrap">付款方式2</th>
+                    <th class="text-nowrap">金額2</th>
+                    <th class="text-nowrap">帳戶/卡號後四碼2</th>
+                    <th class="text-nowrap">付款方式3</th>
+                    <th class="text-nowrap">金額3</th>
+                    <th class="text-nowrap">帳戶/卡號後四碼3</th>
+                    <th class="text-nowrap" >付款日期</th>
+                    <th class="text-nowrap">服務人員</th>
+                    <th class="text-nowrap">備註</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td class="align-middle">王曉明</td>
+                    <td class="align-middle">0912345678</td>
+                    <td class="align-middle">
+                      <div class="form-group m-0">
+                        <select class="custom-select border-0 bg-transparent input_width" name="pay_state">
+                          <option selected disabled value="">付款狀態</option>
+                          <option value="1">完款</option>
+                          <option value="2">付訂</option>
+                          <option value="3">留單</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td class="align-middle"><input type="text" class="border-0 bg-transparent input_width"></td>
+                    <td class="align-middle">1000</td>
+                    <td class="align-middle">500</td>
+                    <td class="align-middle">
+                      <div class="form-group m-0">
+                        <select class="custom-select border-0 bg-transparent input_width" name="pay_way">
+                          <option selected disabled value="">付款方式</option>
+                          <option value="1">現金</option>
+                          <option value="2">匯款</option>
+                          <option value="3">輕鬆付</option>
+                          <option value="4">一次付</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td class="align-middle">1500</td>
+                    <td class="align-middle">1234</td>
+                    <td class="align-middle">
+                      <div class="form-group m-0">
+                        <select class="custom-select border-0 bg-transparent input_width" name="pay_way">
+                          <option selected disabled value="">付款方式</option>
+                          <option value="1">現金</option>
+                          <option value="2">匯款</option>
+                          <option value="3">輕鬆付</option>
+                          <option value="4">一次付</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td class="align-middle"><input type="text" class="border-0 bg-transparent input_width"></td>
+                    <td class="align-middle"><input type="text" class="border-0 bg-transparent input_width"></td>
+                    <td class="align-middle">
+                      <div class="form-group m-0">
+                        <select class="custom-select border-0 bg-transparent input_width" name="pay_way">
+                          <option selected disabled value="">付款方式</option>
+                          <option value="1">現金</option>
+                          <option value="2">匯款</option>
+                          <option value="3">輕鬆付</option>
+                          <option value="4">一次付</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td class="align-middle"><input type="text" class="border-0 bg-transparent input_width"></td>
+                    <td class="align-middle"><input type="text" class="border-0 bg-transparent input_width"></td>
+                    <td class="align-middle"><input type="text" class="border-0 bg-transparent input_width"></td>
+                    <td class="align-middle"><input type="text" class="border-0 bg-transparent input_width"></td>
+                    <td class="align-middle"><input type="text" class="border-0 bg-transparent input_width"></td>
                   </tr>
                   <tr>
                     <td></td>
