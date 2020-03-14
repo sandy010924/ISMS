7f876fbe4a398d4b6e3cea4db6e2aea44cc1b055
sandy010924 <sandy010924@gmail.com>
2020-03-14 23:50:24 +0800

    Model更新

--- laravel/app/Model/Registration.php
+++ laravel/app/Model/Registration.php
@@ -19,6 +19,14 @@ class Registration extends Model
         'sign',                     // 簽名檔案
         'status_payment',           // 付款狀態
         'id_events',                // 場次ID
-        'registration_join'         // 我想參加課程
+        'registration_join',        // 我想參加課程
+        'id_group',                 // 群組ID
+        'pay_date',                 // 統編
+        'pay_memo',                 // 抬頭
+        'person',                   // 服務人員
+        'type_invoice',             // 統一發票
+        'number_taxid',             // 統編
+        'companytitle',             // 抬頭
+        'source_events',            // 來源場次ID
     ];
 }
