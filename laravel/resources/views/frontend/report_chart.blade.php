9bafb44e446c3249e47738c3756c52716654b624
joanna <okokis101@gmail.com>
2020-04-05 03:58:21 +0800

    場次數據搜尋條件

--- laravel/resources/views/frontend/report_chart.blade.php
+++ laravel/resources/views/frontend/report_chart.blade.php
@@ -1,71 +1,303 @@
 @extends('frontend.layouts.master')
 
 @section('title', '課程管理')
-@section('header', '場次數據圖表')
+@section('header', '數據報表')
 
 @section('content')
-
-  @include('components.course_list_chart')
-
-  <!-- 名單列表內容 -->
+<!-- Content Start -->
+    <!--場次數據頁面內容-->
     <div class="card m-3">
       <div class="card-body">
         <div class="row mb-3">
           <div class="col align-self-center">
-            <span class="h5 pl-1">報名名單</span>
+            <!-- <span class="h6">2019/11/01(五) ~ 2019/11/19(二)</span> -->
+            <div class="row">
+              <div class="col-2">
+                <select  class="form-control" id="select_type">
+                  <option value="0">最近30天</option>
+                  <option value="1">銷講</option>
+                  <option value="2">正課</option>
+                  <option value="3">活動</option>
+                </select>
+              </div>
+
+              <div class="col-1.5">
+                <select  class="form-control" id="select_type">
+                  <option value="0">所有老師</option>
+                  <option value="1">Jack</option>
+                  <option value="2">Julia</option>
+                  <option value="3">北極熊</option>
+                </select>
+              </div>
+
+              <div class="col-2">
+                <select  class="form-control" id="select_type">
+                  <option value="0">所有類型</option>
+                  <option value="1">類型一</option>
+                  <option value="2">類型二</option>
+                  <option value="3">類型三</option>
+                </select>
+              </div>
+
+              <div class="col-1.5">
+                <select  class="form-control" id="select_type">
+                  <option value="0">所有課程</option>
+                  <option value="1">課程一</option>
+                  <option value="2">課程二</option>
+                  <option value="3">課程三</option>
+                </select>
+              </div>
+
+              <div class="col-2">
+                <select  class="form-control" id="select_type">
+                  <option value="0">所有地區</option>
+                  <option value="1">地區一</option>
+                  <option value="2">地區二</option>
+                  <option value="3">地區三</option>
+                </select>
+              </div>
+
+              <div class="col-2">
+                <select  class="form-control" id="select_type">
+                  <option value="0">所有時段</option>
+                  <option value="1">時段一</option>
+                  <option value="2">時段二</option>
+                  <option value="3">時段三</option>
+                </select>
+              </div>
+
+              <div class="col-1.5">
+                <select  class="form-control" id="select_type">
+                  <option value="0">所有來源</option>
+                  <option value="1">下午</option>
+                  <option value="2">晚上</option>
+                  <option value="3">整天</option>
+                </select>
+              </div>
+            </div>
+
           </div>
-          <div class="col text-right">
-            <button type="button" class="btn btn-outline-secondary mx-1">匯出名單</button>  
+          <div class="col-1">
+            <button type="button" class="btn btn-outline-secondary btn_date">搜尋</button>
+          </div>
+          <div class="col-1">
+            <button type="button" class="btn btn-outline-secondary btn_date" data-toggle="modal" data-target="#scheduleModal">+比較</button>
           </div>
         </div>
-        <div class="table-responsive">
+        <!-- <div class="table-responsive">
           <table class="table table-striped table-sm text-center">
             <thead>
               <tr>
-                <th>Submission Date</th>
-                <th>名單來源</th>
-                <th>姓名</th>
-                <th>聯絡電話</th>
-                <th>電子郵件</th>
-                <th>目前職業</th>
-                <th>報到</th>
-                <th>付款狀態</th>
+                <th>日期</th>
+                <th>課程名稱</th>
+                <th>場次</th>
+                <th>報名筆數</th>
+                <th>實到人數</th>
+                <th>報到率</th>
+                <th>成交人數</th>
+                <th>成交率</th>
+                <th></th>
               </tr>
             </thead>
-            <tbody id="table_list">
+            <tbody>
               <tr>
-                <td>2020-01-05 20:32:36</td>
-                <td>FB</td>
-                <td>周祥詮</td>
-                <td>0912345436</td>
-                <td>joy@abc.com</td>
-                <td>科技業</td>
-                <td>報到</td>
-                <td>完款</td>
-              </tr>
-              <tr>
-                <td>2020-01-05 15:12:26</td>
-                <td>Line</td>
-                <td>陳建華</td>
-                <td>0932455467</td>
-                <td>aabc@test.com</td>
-                <td>服務業</td>
-                <td>報到</td>
-                <td>付訂</td>
-              </tr>
-              <tr>
-                <td>2020-01-05 10:32:20</td>
-                <td>FB</td>
-                <td>李宜貞</td>
-                <td>0970488096</td>
-                <td>qqss@oxox.com.tw</td>
-                <td>資訊業</td>
-                <td>報到</td>
-                <td>留單</td>
+                <td>2019/11/19(二)</td>
+                <td>零秒成交數</td>
+                <td>台北下午場</td>
+                <td>67/7</td>
+                <td>25</td>
+                <td>41.6%</td>
+                <td>3</td>
+                <td>0.12%</td>
+                <td>
+                  <a href="{{ route('report_chart') }}"><button type="button"
+                      class="btn btn-secondary btn-sm">完整內容</button></a>
+                </td>
               </tr>
             </tbody>
           </table>
-        </div>
+        </div> -->
       </div>
     </div>
+
+
+    <!-- 排成設定Modal -->
+    <div class="modal fade" id="scheduleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document" style="max-width: 70%;">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">數據報表-搜尋條件</h5>
+                <button id="modalAddFilter" type="button" class="ml-2 btn btn-outline-secondary btn_date">新增搜尋條件</button>
+
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div id="dataFilterModal" class="modal-body">
+                <div class="row">
+                  <div class="col-2">
+                    <select  class="form-control" id="select_type">
+                      <option value="0">最近30天</option>
+                      <option value="1">銷講</option>
+                      <option value="2">正課</option>
+                      <option value="3">活動</option>
+                    </select>
+                  </div>
+
+                  <div class="col-1.5">
+                    <select  class="form-control" id="select_type">
+                      <option value="0">所有老師</option>
+                      <option value="1">Jack</option>
+                      <option value="2">Julia</option>
+                      <option value="3">北極熊</option>
+                    </select>
+                  </div>
+
+                  <div class="col-2">
+                    <select  class="form-control" id="select_type">
+                      <option value="0">所有類型</option>
+                      <option value="1">類型一</option>
+                      <option value="2">類型二</option>
+                      <option value="3">類型三</option>
+                    </select>
+                  </div>
+
+                  <div class="col-1.5">
+                    <select  class="form-control" id="select_type">
+                      <option value="0">所有課程</option>
+                      <option value="1">課程一</option>
+                      <option value="2">課程二</option>
+                      <option value="3">課程三</option>
+                    </select>
+                  </div>
+
+                  <div class="col-2">
+                    <select  class="form-control" id="select_type">
+                      <option value="0">所有地區</option>
+                      <option value="1">地區一</option>
+                      <option value="2">地區二</option>
+                      <option value="3">地區三</option>
+                    </select>
+                  </div>
+
+                  <div class="col-2">
+                    <select  class="form-control" id="select_type">
+                      <option value="0">所有時段</option>
+                      <option value="1">時段一</option>
+                      <option value="2">時段二</option>
+                      <option value="3">時段三</option>
+                    </select>
+                  </div>
+
+                  <div class="col-1.5">
+                    <select  class="form-control" id="select_type">
+                      <option value="0">所有來源</option>
+                      <option value="1">下午</option>
+                      <option value="2">晚上</option>
+                      <option value="3">整天</option>
+                    </select>
+                  </div>
+                </div>
+
+              </div>
+              <div class="modal-footer">
+                <button type="button" id="saveScheduleBtn" class="btn btn-secondary" data-dismiss="modal">確定</button>
+                <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+              </div>
+            </div>
+          </div>
+        </div>
+  <!-- Content End -->
+
+  <script>
+    $(document).ready(function () {
+      var filterCount = 0;
+
+
+      $('#modalAddFilter').on('click', function() {
+        filterCount++;
+        if (filterCount <4 ) {
+
+          var str = `
+          <div class="row mt-2">
+                <div class="col-2">
+                  <select  class="form-control" id="select_type">
+                    <option value="0">最近30天</option>
+                    <option value="1">銷講</option>
+                    <option value="2">正課</option>
+                    <option value="3">活動</option>
+                  </select>
+                </div>
+
+                <div class="col-1.5">
+                  <select  class="form-control" id="select_type">
+                    <option value="0">所有老師</option>
+                    <option value="1">Jack</option>
+                    <option value="2">Julia</option>
+                    <option value="3">北極熊</option>
+                  </select>
+                </div>
+
+                <div class="col-2">
+                  <select  class="form-control" id="select_type">
+                    <option value="0">所有類型</option>
+                    <option value="1">類型一</option>
+                    <option value="2">類型二</option>
+                    <option value="3">類型三</option>
+                  </select>
+                </div>
+
+                <div class="col-1.5">
+                  <select  class="form-control" id="select_type">
+                    <option value="0">所有課程</option>
+                    <option value="1">課程一</option>
+                    <option value="2">課程二</option>
+                    <option value="3">課程三</option>
+                  </select>
+                </div>
+
+                <div class="col-2">
+                  <select  class="form-control" id="select_type">
+                    <option value="0">所有地區</option>
+                    <option value="1">地區一</option>
+                    <option value="2">地區二</option>
+                    <option value="3">地區三</option>
+                  </select>
+                </div>
+
+                <div class="col-2">
+                  <select  class="form-control" id="select_type">
+                    <option value="0">所有時段</option>
+                    <option value="1">時段一</option>
+                    <option value="2">時段二</option>
+                    <option value="3">時段三</option>
+                  </select>
+                </div>
+
+                <div class="col-1.5">
+                  <select  class="form-control" id="select_type">
+                    <option value="0">所有來源</option>
+                    <option value="1">下午</option>
+                    <option value="2">晚上</option>
+                    <option value="3">整天</option>
+                  </select>
+                </div>
+              </div>
+
+            </div>
+
+          `;
+
+          $('#dataFilterModal:last').append(str);
+        }else {
+          alert('已達到搜尋條件限制!');
+        }
+      });
+
+
+    });
+
+
+  </script>
+
 @endsection
