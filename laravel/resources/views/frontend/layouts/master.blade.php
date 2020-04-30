a53cc371bc169ea211a524a47ee65da25182bfda
Rocky <goduu10202200@gmail.com>
2020-04-30 20:42:45 +0800

    修改JS連結

--- laravel/resources/views/frontend/layouts/master.blade.php
+++ laravel/resources/views/frontend/layouts/master.blade.php
@@ -91,9 +91,6 @@
   <script src="{{ asset('js/dataTables.buttons.min.js') }}"></script>
   <script src="{{ asset('js/jszip.min.js') }}"></script>
   <script src="{{ asset('js/buttons.html5.min.js') }}"></script>
-  <!-- <script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script> -->
-  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script> -->
-  <!-- <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script> -->
   <!-- Excel 匯出 Rocky (2020/04/30) -->
 </head>
 
