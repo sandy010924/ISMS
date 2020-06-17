e81ba12685c3db430fbc07e621eb1d3c96c53a71
sandy010924 <sandy010924@gmail.com>
2020-06-17 23:22:22 +0800

    場次總覽[查詢名單] 匯出功能

--- laravel/resources/views/components/datatable.blade.php
+++ laravel/resources/views/components/datatable.blade.php
@@ -1,10 +1,12 @@
-<div class="table-responsive">
-  <table id="table_list" name="table_list" class="table table-striped table-sm text-center border rounded-lg">
-    <thead id="tableHead">
-      {{ $thead }}
-    </thead>
-    <tbody id="tableBody">
-      {{ $tbody }}
-    </tbody>
-  </table>
+<div id="datatableDiv">
+  <div class="table-responsive">
+    <table id="table_list" name="table_list" class="table table-striped table-sm text-center border rounded-lg">
+      <thead id="tableHead">
+        {{ $thead }}
+      </thead>
+      <tbody id="tableBody">
+        {{ $tbody }}
+      </tbody>
+    </table>
+  </div>
 </div>
