1dde1d54a367594eb42838cded0ddb50ab78a17b
sandy010924 <sandy010924@gmail.com>
2020-08-17 16:23:52 +0800

    [場次報表]修正新增資料modal層級問題

--- laravel/resources/views/frontend/course_return.blade.php
+++ laravel/resources/views/frontend/course_return.blade.php
@@ -53,7 +53,7 @@
   .modal:nth-of-type(even) {
     z-index: 1052 !important;
   }
-  .modal-backdrop.show:nth-of-type(even) {
+  .modal-backdrop.show:nth-of-type(odd) {
       z-index: 1051 !important;
   }
   .modal { 
