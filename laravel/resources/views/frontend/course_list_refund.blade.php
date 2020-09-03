14175f98a9bd7c59d52c9f20c00e1e8e19018440
sandy010924 <sandy010924@gmail.com>
2020-09-04 05:28:20 +0800

    [退費名單]modal層級

--- laravel/resources/views/frontend/course_list_refund.blade.php
+++ laravel/resources/views/frontend/course_list_refund.blade.php
@@ -225,9 +225,9 @@
       z-index: 1052 !important;
     }
 
-    .modal-backdrop.show:nth-of-type(even) {
+    /* .modal-backdrop.show:nth-of-type(even) {
       z-index: 1051 !important;
-    }
+    } */
 
     .modal {
       overflow-y: auto !important;
